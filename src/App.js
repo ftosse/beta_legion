b7c3ea3d6eebd0c41f17dfd08b9d8d61ef27edb9
ftosse <felipeat95gmail.com>
2022-02-02 23:35:55 -0500

    Integracion con firebase-react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          pagina legiongroups firebase <code>src/App.js</code> colombia binaria.
         </p>
         <a
           className="App-link"
